1083dc3e546cbcda1f134d42971f119e19a7fa88
Wout-Motmans <91081085+Wout-Motmans@users.noreply.github.com>
2024-06-06 13:41:11 +0200

    added grub skip bootloader menu lines

--- setup_debian.sh
+++ setup_debian.sh
@@ -16,7 +16,9 @@ HASHPW=$(echo -e "$passwd\n$passwd" | LC_ALL=C /usr/bin/grub-mkpasswd-pbkdf2 | a
 echo "set superusers=root" | tee -a /etc/grub.d/40_custom
 echo "password_pbkdf2 root $HASHPW" | tee -a /etc/grub.d/40_custom
 
-sudo sed -i '/^CLASS=/ s/"$/ --unrestricted"/' /etc/grub.d/10_linux
+sed -i '/^CLASS=/ s/"$/ --unrestricted"/' /etc/grub.d/10_linux
+sed -i '/GRUB_TIMEOUT/c\GRUB_TIMEOUT\=0' /etc/default/grub
+sed -i '/GRUB_DEFAULT/iGRUB_DISABLE_SUBMENU\=y' /etc/default/grub
 
 update-grub
 
