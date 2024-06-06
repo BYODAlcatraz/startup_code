6fca323de666f3aa24da6076a60f3d7c5bc15012
Rafdadoumont <72247088+Rafdadoumont@users.noreply.github.com>
2024-06-06 21:51:13 +0200

    Wallpaper warden

--- setup_wallpaper.sh
+++ setup_wallpaper.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 cd /home/warden/startup_code/
-su - prisoner -c "mkdir /home/prisoner/Pictures"
+su - prisoner -c "mkdir -p /home/prisoner/Pictures"
 cp /home/warden/startup_code/wallpaper_exam.png /home/prisoner/Pictures
 su - prisoner -c "kwriteconfig5 --file '/home/prisoner/.config/plasma-org.kde.plasma.desktop-appletsrc'\
  --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General'\
  --key 'Image' '/home/prisoner/Pictures/wallpaper_exam.png'"
+
+su - warden -c "mkdir -p /home/warden/Pictures"
+cp /home/warden/startup_code/wallpaper_exam.png /home/warden/Pictures
+su - warden -c "kwriteconfig5 --file '/home/warden/.config/plasma-org.kde.plasma.desktop-appletsrc'\
+ --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General'\
+ --key 'Image' '/home/warden/Pictures/wallpaper_exam.png'"
