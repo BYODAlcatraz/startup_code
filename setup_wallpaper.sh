59f49a77bf03592c25c325563e991ec6017b8ec8
Rafdadoumont <72247088+Rafdadoumont@users.noreply.github.com>
2024-06-06 15:57:48 +0200

    Change exec user

--- setup_wallpaper.sh
+++ setup_wallpaper.sh
@@ -2,7 +2,7 @@
 
 cd /home/warden/startup_code/
 su - prisoner -c "mkdir /home/prisoner/Pictures"
-su - prisoner -c "cp /home/warden/startup_code/wallpaper_exam.png /home/prisoner/Pictures"
+cp /home/warden/startup_code/wallpaper_exam.png /home/prisoner/Pictures
 su - prisoner -c "kwriteconfig5 --file '/home/prisoner/.config/plasma-org.kde.plasma.desktop-appletsrc'\
  --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General'\
  --key 'Image' '/home/prisoner/Pictures/wallpaper_exam.png'"
