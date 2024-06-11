a909e0a926460c327c87412d717311e65d1529ac
Wout-Motmans <91081085+Wout-Motmans@users.noreply.github.com>
2024-06-11 16:34:39 +0200

    Update setup_wallpaper.sh

--- setup_wallpaper.sh
+++ setup_wallpaper.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+mkdir -p /etc/dconf/profile
+mkdir -p /etc/dconf/db/local.d
 
-cd /home/warden/startup_code/
-su - prisoner -c "mkdir -p /home/prisoner/Pictures"
-cp /home/warden/startup_code/wallpaper_exam.png /home/prisoner/Pictures
-su - prisoner -c "kwriteconfig5 --file '/home/prisoner/.config/plasma-org.kde.plasma.desktop-appletsrc'\
- --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General'\
- --key 'Image' '/home/prisoner/Pictures/wallpaper_exam.png'"
 
-su - warden -c "mkdir -p /home/warden/Pictures"
-cp /home/warden/startup_code/wallpaper_exam.png /home/warden/Pictures
-su - warden -c "kwriteconfig5 --file '/home/warden/.config/plasma-org.kde.plasma.desktop-appletsrc'\
- --group 'Containments' --group '1' --group 'Wallpaper' --group 'org.kde.image' --group 'General'\
- --key 'Image' '/home/warden/Pictures/wallpaper_exam.png'"
+echo "user-db:user
+system-db:local" > /etc/dconf/profile/user
+
+echo "[org/gnome/desktop/background]
+picture-uri='file:///user/share/wallpapers/wallpaper_exam.png'" > /etc/dconf/db/local.d/00-wallpaper
+
+chmod 644 /usr/share/wallpapers/wallpaper_exam.png
+
+dconf update
