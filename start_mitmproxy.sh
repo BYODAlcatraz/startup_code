8a0ac3a7c871f18437aadbb33d5281ab19c029ab
Rafdadoumont <72247088+Rafdadoumont@users.noreply.github.com>
2024-06-12 12:19:18 +0200

    Start mitmproxy

---
+++ start_mitmproxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Verifieer of de gebruiker root is
+if [ "$(id -u)" -ne 0 ]; then
+        echo 'Script must be ran by root'>&2
+        exit 1
+fi
+
+# Herstel iptables regels
+iptables-restore < ~/rules.v4
+ip6tables-restore < ~/rules.v6
+
+# Start de mitmproxy
+mitmdump --mode transparent &
