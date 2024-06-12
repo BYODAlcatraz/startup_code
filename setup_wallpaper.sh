bff3a929557ec421c36270ad8202d65806d99956
Wout-Motmans <91081085+Wout-Motmans@users.noreply.github.com>
2024-06-12 09:59:39 +0200

    Update setup_wallpaper.sh

--- setup_wallpaper.sh
+++ setup_wallpaper.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-su student -c gsettings get org.gnome.desktop.background picture-uri 'file:///usr/share/wallpapers/wallpaper_exam.png'
-
-
-# mkdir -p /etc/dconf/profile
-# mkdir -p /etc/dconf/db/local.d
-
-
-# echo "user-db:user
-# system-db:local" > /etc/dconf/profile/user
-
-# echo "[org/gnome/desktop/background]
-# picture-uri='file:///user/share/wallpapers/wallpaper_exam.png'" > /etc/dconf/db/local.d/00-wallpaper
-
-# chmod 644 /usr/share/wallpapers/wallpaper_exam.png
-
-# dconf update
+su student -c "gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/wallpapers/wallpaper_exam.png'"
