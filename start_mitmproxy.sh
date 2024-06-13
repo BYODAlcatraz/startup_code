1c7bda338753a5d9626a6f911439554932e69450
Wout-Motmans <91081085+Wout-Motmans@users.noreply.github.com>
2024-06-13 10:19:03 +0200

    Update start_mitmproxy.sh

--- start_mitmproxy.sh
+++ start_mitmproxy.sh
@@ -6,9 +6,35 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
-# Herstel iptables regels
-iptables-restore < /root/rules.v4
-ip6tables-restore < /root/rules.v6
+
+# Flush iptables
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+ip6tables -F
+ip6tables -X
+ip6tables -t nat -F
+ip6tables -t nat -X
+ip6tables -P INPUT ACCEPT
+ip6tables -P OUTPUT ACCEPT
+ip6tables -P FORWARD ACCEPT
+
+# Redirect alles naar mitm
+iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner student -j REDIRECT --to-port 8080
+iptables -t nat -A OUTPUT -p udp -m owner --uid-owner student -j REDIRECT --to-port 8080 ! --dport 53
+iptables -t nat -A OUTPUT -p icmp -m owner --uid-owner student -j REDIRECT --to-port 8080
+
+ip6tables -t nat -A OUTPUT -p tcp -m owner --uid-owner student -j REDIRECT --to-port 8080
+ip6tables -t nat -A OUTPUT -p udp -m owner --uid-owner student -j REDIRECT --to-port 8080 ! --dport 53
+ip6tables -t nat -A OUTPUT -p icmp -m owner --uid-owner student -j REDIRECT --to-port 8080
+
+# iptables-restore < /root/rules.v4
+# ip6tables-restore < /root/rules.v6
 
 # Start de mitmproxy
 #chmod +x /home/warden/startup_code/start_mitm.sh
