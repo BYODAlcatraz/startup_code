07d05fadbc71211c8f8dac42c0ec34b702bdea3c
Wout-Motmans <91081085+Wout-Motmans@users.noreply.github.com>
2024-06-13 10:20:05 +0200

    Update setup_mitmproxy.sh

--- setup_mitmproxy.sh
+++ setup_mitmproxy.sh
@@ -16,35 +16,35 @@ apt install -y iptables
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
 
-# Flush iptables
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
+# # Flush iptables
+# iptables -F
+# iptables -X
+# iptables -t nat -F
+# iptables -t nat -X
+# iptables -P INPUT ACCEPT
+# iptables -P OUTPUT ACCEPT
+# iptables -P FORWARD ACCEPT
 
-ip6tables -F
-ip6tables -X
-ip6tables -t nat -F
-ip6tables -t nat -X
-ip6tables -P INPUT ACCEPT
-ip6tables -P OUTPUT ACCEPT
-ip6tables -P FORWARD ACCEPT
+# ip6tables -F
+# ip6tables -X
+# ip6tables -t nat -F
+# ip6tables -t nat -X
+# ip6tables -P INPUT ACCEPT
+# ip6tables -P OUTPUT ACCEPT
+# ip6tables -P FORWARD ACCEPT
 
-# Redirect alles naar mitm
-iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner student -j REDIRECT --to-port 8080
-iptables -t nat -A OUTPUT -p udp -m owner --uid-owner student -j REDIRECT --to-port 8080 ! --dport 53
-iptables -t nat -A OUTPUT -p icmp -m owner --uid-owner student -j REDIRECT --to-port 8080
+# # Redirect alles naar mitm
+# iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner student -j REDIRECT --to-port 8080
+# iptables -t nat -A OUTPUT -p udp -m owner --uid-owner student -j REDIRECT --to-port 8080 ! --dport 53
+# iptables -t nat -A OUTPUT -p icmp -m owner --uid-owner student -j REDIRECT --to-port 8080
 
-ip6tables -t nat -A OUTPUT -p tcp -m owner --uid-owner student -j REDIRECT --to-port 8080
-ip6tables -t nat -A OUTPUT -p udp -m owner --uid-owner student -j REDIRECT --to-port 8080 ! --dport 53
-ip6tables -t nat -A OUTPUT -p icmp -m owner --uid-owner student -j REDIRECT --to-port 8080
+# ip6tables -t nat -A OUTPUT -p tcp -m owner --uid-owner student -j REDIRECT --to-port 8080
+# ip6tables -t nat -A OUTPUT -p udp -m owner --uid-owner student -j REDIRECT --to-port 8080 ! --dport 53
+# ip6tables -t nat -A OUTPUT -p icmp -m owner --uid-owner student -j REDIRECT --to-port 8080
 
-# Save rules to file
-iptables-save > /root/rules.v4
-ip6tables-save > /root/rules.v6
+# # Save rules to file
+# iptables-save > /root/rules.v4
+# ip6tables-save > /root/rules.v6
 
 # Start mitmproxy om configuratie bestanden te laten genereren
 mitmdump --mode transparent &
