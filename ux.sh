d9e79400bd9baf156d17f07c3d22248407fe81ab
Wout-Motmans <91081085+Wout-Motmans@users.noreply.github.com>
2024-06-13 14:41:05 +0200

    Update ux.sh

--- ux.sh
+++ ux.sh
@@ -3,6 +3,10 @@
 # enable dock settings extension
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
 
+#Set the right keyboard layouts
+gsettings reset org.gnome.desktop.input-sources sources
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'be'), ('xkb', 'us')]"
+
 # lock dock to bottom
 gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
